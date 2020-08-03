9123318e06453038f2b8db455ee7edb8a6ded214
SpencerOfwiti <maxspencer56@gmail.com>
2020-08-03 11:52:14 +0300

    Add routing and page setup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Role} from "./_models";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular-Client-Side';
+  isAdmin: boolean = false;
+  isLoggedIn: boolean = false;
+  currentUser: any;
+
+  ngOnInit(): void {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (this.currentUser !== null) {
+      this.isLoggedIn = true;
+      if (this.currentUser.role === Role.Admin) {
+        this.isAdmin = true;
+      }
+    }
+    console.log(this.currentUser);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from "./shared/shared.module";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AuthenticationService, OrganizationService, PaymentService} from "./_services";
+import {AuthGuard} from "./_guards";
+import {RoleGuard} from "./_guards";
+import {ErrorInterceptor} from "./_helpers";
+import {JwtInterceptor} from "./_helpers";
 
 @NgModule({
   declarations: [
@@ -10,9 +17,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    AuthenticationService,
+    OrganizationService,
+    PaymentService,
+    AuthGuard,
+    RoleGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
